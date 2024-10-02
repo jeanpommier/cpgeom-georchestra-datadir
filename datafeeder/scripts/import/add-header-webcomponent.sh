a2613883340ae2b854f62f0049b735bd9b8cb790
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2024-10-02 16:47:31 +0200

    Config for occ.pigeosolutions.fr

--- datafeeder/scripts/import/add-header-webcomponent.sh
+++ datafeeder/scripts/import/add-header-webcomponent.sh
@@ -4,7 +4,8 @@
 # so you need to add it with chmod and commit/push it
 
 DATAFEEDER_DIR=${1:-/usr/share/nginx/html}
-SNIPPET="<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header active-app='import' style='height:90px'></geor-header>"
+# SNIPPET="<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header active-app='import' style='height:90px'></geor-header>"
+SNIPPET='<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header style="width:100%;height:90px;border:none;" active-app="import" legacy-url="/header/" legacy-header="false" logo-url="https://www.regions-departements-france.fr/images/regions/drapeau/drapeau-region-occitanie-300.jpg"></geor-header>'
 
 if grep -q "${SNIPPET}" "${DATAFEEDER_DIR}/index.html"; then
   echo "[INFO] geOrchestra: header already present."
--- datahub/scripts/add-header-webcomponent.sh
+++ datahub/scripts/add-header-webcomponent.sh
@@ -4,7 +4,8 @@
 # so you need to add it with chmod and commit/push it
 
 DATAHUB_DIR=${1:-/usr/share/nginx/html/datahub}
-SNIPPET="<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header active-app='datahub' style='height:90px'></geor-header>"
+# SNIPPET="<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header active-app='datahub' style='height:90px'></geor-header>"
+SNIPPET='<script src='https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js'></script><geor-header style="width:100%;height:90px;border:none;" active-app="datahub" legacy-url="/header/" legacy-header="false" logo-url="https://www.regions-departements-france.fr/images/regions/drapeau/drapeau-region-occitanie-300.jpg"></geor-header>'
 
 if grep -q "${SNIPPET}" "${DATAHUB_DIR}/index.html"; then
   echo "[INFO] geOrchestra: header already present."
